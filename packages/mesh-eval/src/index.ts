3f1c42ec8ac640527a22ff5981be5bccff65d548
Deji Odetayo <deji@corelay.dev>
2026-04-24 02:01:56 +0100

    feat(mesh-eval): public barrel

---
+++ packages/mesh-eval/src/index.ts
@@ -0,0 +1,19 @@
+export { runEval } from "./run.js";
+export type { RunEvalOptions } from "./run.js";
+export { evaluateAssertion } from "./assertions.js";
+export { createLlmJudge } from "./judge.js";
+export type { CreateLlmJudgeOptions } from "./judge.js";
+export type {
+  Assertion,
+  AssertionResult,
+  CaseResult,
+  ContainsAssertion,
+  EvalCase,
+  EvalJudge,
+  EvalReport,
+  EvalSuite,
+  EvalTarget,
+  JudgedAssertion,
+  MatchesAssertion,
+  NotContainsAssertion,
+} from "./types.js";
